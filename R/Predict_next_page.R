2b099a3e2bf7f955fbc663855d4c2b2fd8b855a9
paullenssen <paul@happyidiots.nl>
2019-06-20 16:09:45 +0200

    add import

--- R/Predict_next_page.R
+++ R/Predict_next_page.R
@@ -1,3 +1,10 @@
+#' Take some pageviews, output Markov model prediction
+#'
+#' @param current_URL A character vector of pageview names
+#'
+#' @return The prediction
+#' @import clickstream
+#' @export
 PredictNextPage <- function (current_URL){
   startPattern <- new("Pattern", current_URL)
   out <- try(predict(model, startPattern), silent = TRUE)
